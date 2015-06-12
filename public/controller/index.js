60971daa371b4d0f9b95456bc8483c1d4d955227
d00mz <k.zwier@gmx.de>
2015-06-12 16:51:29 +0200

    Gugg e mol die updates o

--- public/controller/index.js
+++ public/controller/index.js
@@ -1,5 +1,57 @@
 var index = ['$scope','$http',function($scope,$http){
-	$http.get('/airlines').success(function(){
+
+	$scope.emissions = {};
+	$scope.airlines = [];
+	$scope.aircrafts = [];
+
+	$http.get('/airlines').success(function(result,status){
+		console.log(result);
+		console.log(status);
+
+		$scope.airlines = result;
+		console.log($scope.airlines.length);
+		var test = 0;
+		for(var i in $scope.airlines){
+			test += 1;
+		}
+		console.log(test);
 		// alert('hahahah')
-	})
+	});
+	$http.get('/overview').success(function(result,status){
+		console.log(result);
+		console.log(status);
+
+		$scope.aircrafts = result;
+		console.log($scope.aircrafts.length);
+		var test = 0;
+		for(var i in $scope.aircrafts){
+			test += 1;
+		}
+		console.log(test);
+		// alert('hahahah')
+	});
+	$http.get('/emissions').success(function(result,status){
+		$scope.emissions = result;
+		//$scope.initClock();
+		// alert('hahahah')
+	});
+
+	$scope.initCarousel = function(){
+		console.log('test');
+	}
+
+	$scope.hallowelt = function(){
+
+		console.log('hallo');
+
+		var counter = 0;
+		setTimeout(function(){
+			 var interval = setInterval(function() {
+				clock.increment();
+				counter += 1;
+			}, 500);
+		}, 1000);
+	};
+
+
 }];
---
+++ public/directives/directives.js
@@ -0,0 +1,10 @@
+app.directive('onFinishRender',['$timeout', function ($timeout) {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attr) {
+			if (scope.$last === true) {
+				scope.$evalAsync(attr.onFinishRender);
+			}
+		}
+	};
+}]);
