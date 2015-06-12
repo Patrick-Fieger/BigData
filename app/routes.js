ab0d2553ee8a6e2a14d63d2ff89e1de9db4ef47b
d00mz <k.zwier@gmx.de>
2015-06-12 12:39:02 +0200

    Airline (Anzahl Flieger gesamt, boden, luft) & geschätzter Verbrauch

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,5 @@ module.exports = function(app){
 	app.get('/data', flight.getData);
 	app.get('/airlines', flight.getAirlines);
 	app.get('/overview', flight.getSimpleData);
+	app.get('/emissions', flight.getEmissions);
 }
