0ee5f625719d88b517e426a59d9b52748251ae36
Patrick Fieger <patrickfieger90@gmail.com>
2015-06-29 15:59:34 +0200

    ground fix

--- public/controller/index.js
+++ public/controller/index.js
@@ -11,41 +11,37 @@ var index = ['$scope','$http','planeGraph',function($scope,$http,planeGraph){
 
 	$http.get('/airlines').success(function(result,status){
 
-		$scope.airlines = result;
-
 		var prevent = {}
+		for(var a in result) {
 
-		for(var a in $scope.airlines) {
-			for(var i = 0; i < $scope.airlines[a].planes.ground.length; i++){
-				console.log($scope.airlines[a].planes.ground[i][13] + ': ' + prevent[$scope.airlines[a].planes.ground[i][13]]);
-				if(!prevent[$scope.airlines[a].planes.ground[i][13]]){
-					prevent[$scope.airlines[a].planes.ground[i][13]] = 1;
-					
-					planesArray.push({
-						id: $scope.airlines[a].planes.ground[i][13],
-						label: $scope.airlines[a].planes.ground[i][13],
+
+			
+			for(var i = 0; i < result[a].planes.ground.length; i++){
+				var planeground = result[a].planes.ground[i][13];
+				if(!prevent[planeground]){
+					prevent[planeground] = i;
+					var obj = {
+						id: planeground,
+						label: planeground,
 						x: Math.floor((Math.random() * 30) + 1),
 						y: Math.floor((Math.random() * 30) + 1),
 						size: Math.floor((Math.random() * 5) + 1)
-					})
+					}
 
-					//s.graph.addNode(planesArray[i]);
-
-				} else {
-					console.log(prevent[$scope.airlines[a].planes.ground[i][13]]);
+					planesArray.push(obj);
+					s.graph.addNode(obj);
 				}
-
-
-				if(typeof $scope.planeTypes[$scope.airlines[a].planes.ground[i][8]] === 'undefined') $scope.planeTypes[$scope.airlines[a].planes.ground[i][8]] = 0;
-				$scope.planeTypes[$scope.airlines[a].planes.ground[i][8]] += 1;
 			}
+
+			
+
 		}
 
 
 		//$scope.planes = planesArray;
-		console.log($scope.planes);
-		console.log($scope.airlines);
-		console.log($scope.planeTypes);
+		// console.log($scope.planes);
+		//console.log($scope.airlines);
+		// console.log($scope.planeTypes);
 
 		/*s.graph.addEdge({
 		id: 'e0',
